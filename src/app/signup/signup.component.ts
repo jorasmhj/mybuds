9d9ad3c98d01a80de8a2a01eb29610ba4669dc54
jorasmhj <jorasmhj@gmail.com>
2018-10-23 22:24:02 +0545

    implemented material

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 import { User } from '../model/user';
 import { UserService } from '../services/user.service';
 import { Router } from '@angular/router';
 import { Title } from '@angular/platform-browser';
+import { moveIn, fallIn } from '../animation';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
+  animations: [moveIn()]
 })
 export class SignupComponent implements OnInit {
+  @HostBinding('@moveIn')
   user: User = new User();
   conformPassword: string;
 
