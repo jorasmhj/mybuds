54a5b8f5753d9f7278bcdf390795b834f2e74067
jorasmhj <jorasmhj@gmail.com>
2018-10-23 19:42:47 +0545

    fix

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 import path from 'path';
 
-app.use(static(__dirname + '/dist/explorer/'));
-
-app.listen(process.env.PORT || 8080);
+app.use(express.static(__dirname + '/dist/explorer/'));
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/explorer/index.html'));
 });
+
+app.listen(process.env.PORT || 8080);
