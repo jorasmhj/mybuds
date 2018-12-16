2fe9f44bb16a681b02ac83d55da7b6cfc51d811a
joras <jorasmhj@gmail.com>
2018-12-16 17:47:04 +0545

    added a bunch

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require('path');
+const compression = require('compression');
 const app = express();
+
+app.use(compression);
 
 app.use(express.static(__dirname + '/dist/explorer'));
 
-app.get('/*', function(req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname + '/dist/explorer/index.html'));
 });
 
